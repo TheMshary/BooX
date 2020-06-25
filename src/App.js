5cd6ffecb85230dbc693aad6a795eea2505b7b9a
TheMshary <themshary94@gmail.com>
2020-06-25 14:19:25 +0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styles from "./styles";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <img
+        src="https://pbs.twimg.com/profile_images/1090824731752771584/yuJ1_jwV_400x400.jpg"
+        style={styles.logo}
+      />
+      <p style={{ textAlign: "center" }}>Where Books Are X'ed!</p>
+      <hr />
+      <h2>Meditations - Hardcover</h2>
+      <img
+        src="https://images-na.ssl-images-amazon.com/images/I/41lHhcjiDgL._SX311_BO1,204,203,200_.jpg"
+        style={styles.bookImage}
+      />
+      <p>Marcus Aurelius</p>
+      <p>$3.44</p>
+      <hr />
+      <h2>12 Rules For Life - Hardcover</h2>
+      <img
+        src="https://images-na.ssl-images-amazon.com/images/I/71qWDVkNMRL.jpg"
+        style={styles.bookImage}
+      />
+      <p>Jordan Peterson</p>
+      <p>$14.95</p>
+      <hr />
+      <h2>The Psychopath Test - Paperback</h2>
+      <img
+        src="https://images-na.ssl-images-amazon.com/images/I/71JYUXRW0cL.jpg"
+        style={styles.bookImage}
+      />
+      <p>Jon Ronson</p>
+      <p>$7.55</p>
     </div>
   );
 }
---
+++ src/styles.js
@@ -0,0 +1,15 @@
+const styles = {
+  logo: {
+    display: "block",
+    marginLeft: "auto",
+    marginRight: "auto",
+    width: "20%",
+  },
+  bookImage: {
+    display: "block",
+    marginRight: "auto",
+    width: "20%",
+  },
+};
+
+export default styles;
